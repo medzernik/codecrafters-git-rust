4986a5419ac5bbb76632ed6cd51f5d4505a09263
medzernik <2439101-medzernik@users.noreply.gitlab.com>
2025-02-09 15:39:15 +0100

    split by lines

--- src/git_tree.rs
+++ src/git_tree.rs
@@ -1,10 +1,4 @@
-use flate2::bufread::ZlibEncoder;
-use flate2::read::ZlibDecoder;
-use flate2::Compression;
-use std::{
-    io::{BufRead, Read},
-    path::Path,
-};
+use std::path::Path;
 
 use crate::GitObjectOperations;
 
@@ -26,7 +20,10 @@ pub struct Entry {
 #[derive(Debug)]
 pub struct Tree {
     contents: Vec<Entry>,
+    size: u32,
 }
+
+const BYTE_HASH_SIZE_SPLIT: usize = 21;
 
 impl GitObjectOperations for Tree {
     fn new(hash: &str) -> Self {
@@ -34,23 +31,29 @@ impl GitObjectOperations for Tree {
         let file = std::fs::read(Path::new(&format!(".git/objects/{dir}/{file}")))
             .expect("cannot open fs file Tree");
         let data = Tree::decode_reader(&file);
-        let split: Vec<String> = data
-            .split(|x| *x == 0)
-            .map(|slice| String::from_utf8_lossy(slice).to_string())
-            .collect();
-        println! {"data: {split:#?}"};
+        let split: Vec<Vec<u8>> = data.splitn(2, |x| *x == 0).map(|x| x.to_vec()).collect();
 
         //header
-        let header = split
-            .first()
-            .unwrap()
-            .split_whitespace()
-            .collect::<Vec<&str>>();
+        let header = String::from_utf8_lossy(&split[0]).to_string();
+        let header: Vec<&str> = header.split_whitespace().collect();
 
         if header[0] != "tree" || header.len() != 2 {
             panic!("this is not a tree file!");
         }
         println!("size of the file is: {}", header[1]);
+
+        // Get each line
+        let mut lines = vec![];
+        let mut line = 0;
+
+        for (i, num) in split[1].iter().enumerate() {
+            if *num == 0 {
+                println!("found the symbol at: {i}, {num}");
+                lines.push(&split[1][line..i + BYTE_HASH_SIZE_SPLIT]);
+                line = i + BYTE_HASH_SIZE_SPLIT;
+                lines.iter().for_each(|x| println!("{:#?}", *x));
+            }
+        }
 
         todo!()
     }
